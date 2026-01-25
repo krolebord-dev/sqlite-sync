82a73f4420047d95af222da69571664f3733158b
krolebord <53054887+krolebord@users.noreply.github.com>
2026-01-25 19:39:15 +0100

    [example]: fix types

--- apps/example/src/server/event-log-server.ts
+++ apps/example/src/server/event-log-server.ts
@@ -11,20 +11,18 @@ export class EventLogServer extends Server<Env> {
   private remoteHandler: RemoteHandler = null!;
 
   onStart(): void | Promise<void> {
-    const { crdtStorage } = durableObjectAdapter.createCrdtStorage({
+    const { remoteHandler } = durableObjectAdapter.createCrdtStorage({
+      batchSize: 100,
       crdtEventsTable: "crdt_events",
       syncDbSchema,
       storage: this.ctx.storage,
       mode: "store-event-log-only",
-    });
-
-    this.remoteHandler = durableObjectAdapter.createRemoteHandler({
-      bufferSize: 100,
-      crdtStorage,
       broadcastPayload: (payload) => {
         this.broadcast(payload);
       },
     });
+
+    this.remoteHandler = remoteHandler;
   }
 
   onMessage(connection: Connection, message: string) {
